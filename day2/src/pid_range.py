4a7c51b6e608794703b5715486d86ee595714e2f
reid-johnson2432 <reid.johnson2432@gmail.com>
2025-12-02 20:19:28 -0500

    Solve part 2 day 2

--- day2/src/pid_range.py
+++ day2/src/pid_range.py
@@ -1,3 +1,6 @@
+import math
+
+
 class PidRange:
     def __init__(self, lower_bound: int, upper_bound: int):
         self.lb = lower_bound
@@ -11,11 +14,25 @@ class PidRange:
     def find_invalid_ids(self):
         for pid in range(self.lb, self.ub + 1):
             check_pid = str(pid)
-            if 0 != len(check_pid) % 2:
+            if 1 == len(check_pid):
                 continue
-            max_substring_len = int(len(check_pid) / 2)
-            this_substring = check_pid[:max_substring_len]
-            next_substring = check_pid[max_substring_len:]   
-            if this_substring == next_substring:
-                self.invalid_ids.append(pid)
-                print(f"Found [{this_substring}] in [{pid}]")
+            factors = self.get_unique_factors(len(check_pid))
+            for divisor in factors:
+                this_substring = check_pid[:divisor]
+                if check_pid == this_substring * int(len(check_pid) / divisor):
+                    self.invalid_ids.append(pid)
+                    print(f"Found [{this_substring}] in [{pid}]")
+                    break
+
+    @staticmethod
+    def get_unique_factors(number):
+        factors = list([1])
+        max_divisor = math.sqrt(number)
+        if 0 != max_divisor % max_divisor:
+            max_divisor = int(max_divisor) + 1
+        for divisor in range(2, int(max_divisor) + 1):
+            if 0 == number % divisor:
+                quotient = int(number / divisor)
+                factors.append(divisor)
+                factors.append(quotient)
+        return factors
